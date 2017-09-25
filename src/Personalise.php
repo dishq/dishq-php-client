d55417204c0f985decbf36e7b3ed028db7f5d064
Viquas <ahmedviquas@MacBook-Pro.local>
2017-09-25 22:44:33 +0530

    Changes in personalise api
    Restaurant id changed to Restaurant array

--- src/Personalise.php
+++ src/Personalise.php
@@ -6,21 +6,48 @@ class Personalise extends Entity
 {
 
 
-    public function get($user_id,$restaurant_id)
+    public function get($user_id,$restaurant_array)
     {
-      if($user_id === NULL || $user_id === '' || $restaurant_id === NULL || $restaurant_id === ''){
-       $error = [ 'message' => 'Input paratmeter missing', 'response' => 'error' ];
+
+
+      if($user_id === NULL || $user_id === ''){
+       $error = [ 'message' => 'User id is missing', 'response' => 'error' ];
          echo json_encode($error);
-      }else{
-        if(!is_integer($user_id) || !is_integer($restaurant_id)) {
-          $error = [ 'message' => 'User id and restaurant id are integers', 'response' => 'error' ];
-           echo json_encode($error);
-        }else{
-          $id ='';
-          $relativeUrl = 'recommend/menu/?user_id='.$user_id.'&restaurant_id='.$restaurant_id;
-          return parent::fetch($id,$relativeUrl);
-        }
+         return ;
       }
 
+      if(!is_integer($user_id)) {
+        $error = [ 'message' => 'User id is not an integer', 'response' => 'error' ];
+         echo json_encode($error);
+         return;
+      }
+
+
+      if(!is_array($restaurant_array)){
+        $error = [ 'message' => 'Restaurant array has to be of type array', 'response' => 'error' ];
+          echo json_encode($error);
+          return;
+      }
+
+      if(empty($restaurant_array)){
+        $error = [ 'message' => 'Restaurant array is empty', 'response' => 'error' ];
+          echo json_encode($error);
+          return;
+      }
+      if(!($this->all_integer($restaurant_array,'is_int'))){
+        $error = [ 'message' => 'Restaurant array should have all integers', 'response' => 'error' ];
+          echo json_encode($error);
+          return;
+
+      }
+      $id ='';
+      print
+              $relativeUrl = 'recommend/menu/?user_id='.$user_id.'&restaurant_id='.join(',',$restaurant_array);
+              return parent::fetch($id,$relativeUrl);
+
+    }
+
+    public function all_integer($array, $predicate) {
+    return array_filter($array, $predicate) === $array;
     }
 }
